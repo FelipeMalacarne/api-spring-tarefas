cbfa2d4994fadc3011d10c4c3dd4d6c49a2ea976
FelipeMalacarne <felipemalacarne012@gmail.com>
2024-05-21 21:35:08 -0300

    Tarefa repository

---
+++ src/main/java/felipe/mateus/tarefas/repositories/TarefaRepository.java
@@ -0,0 +1,9 @@
+package felipe.mateus.tarefas.repositories;
+
+import felipe.mateus.tarefas.entities.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+}
