3ff8e2f05e200101add2834323256c87786dd6f7
FelipeMalacarne <felipemalacarne012@gmail.com>
2024-05-27 22:59:53 -0300

    main test class

---
+++ src/test/java/felipe/mateus/tarefas/TarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package felipe.mateus.tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
